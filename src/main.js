4cb6b6b907ad70880f6c1bfe633e787e83f73d46
sakura <1005833@qq.com>
2020-04-15 17:06:32 +0800

    4.15

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import router from '@/router'
 import components from '@/components'
 // 导入axios
 import axios from '@/api/axios'
+// 缩放、拖拽
+import VueDragResize from 'vue-drag-resize'
 
 // 倒入全局样式
 import '@/styles/index.less'
@@ -20,6 +22,8 @@ Vue.prototype.$http = axios
 Vue.use(EleMent)
 
 Vue.config.productionTip = false
+
+Vue.component('vue-drag-resize', VueDragResize)
 
 new Vue({
   router,
